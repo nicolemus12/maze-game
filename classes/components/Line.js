ac657482abfd89dbb8c2e01cba912f4cf761e56c
nicolemuswere <muswerenicole@gmail.com>
2025-11-16 18:19:35 +0000

    added comments

--- classes/components/Line.js
+++ classes/components/Line.js
@@ -1,3 +1,8 @@
+/*
+ This file contains the Line class which defines the properties and methods of a line. 
+ It has the starting x and y coordinates and the ending x and y coordinates. It also 
+ has methods to get the length of the line and to draw the line.
+*/
 class Line {
 
     constructor(x1, y1, x2, y2) {
--- classes/components/Maze.js
+++ classes/components/Maze.js
@@ -1,4 +1,9 @@
 class Maze {
+    /* This file contains the Maze class. This class is for setting up the lines for 
+    the maze and drawing out the maze layout. The class contains a method called 
+    drawMaze which loops through the array of lines and draws them. There is also 
+    a getLine method which returns the line.
+    */
     constructor() {
         this.lines = [];
         this.lines.push(new Line(120, 2, 120, 350));
@@ -11,11 +16,12 @@ class Maze {
         this.lines.push(new Line(120, 430, 0, 430));
     }
 
+    // loops through the array of lines 
     drawMaze() {
         //   MAZE 1
         strokeWeight(6);
 
-        for (let i= 0; i < this.lines.length; i++){
+        for (let i = 0; i < this.lines.length; i++) {
             this.lines[i].drawLine();
         }
 
@@ -26,3 +32,27 @@ class Maze {
     }
 
 }
+
+//      MAZE 2
+//   strokeWeight(6);
+//    line(120, 2, 120, 100);
+//   line(300, 80, 300, 180);
+//   line(200, 300, 200, 410);
+//    line(290, 380, 290, 500);
+//   line(80, 400, 80, 500);
+
+//   line(400, 180, 0, 180);
+//   line(500, 300, 100, 300);
+//   line(500, 440, 385, 440);
+
+// //   MAZE 3
+// strokeWeight(6);
+// line(100, 90, 0, 90);
+// line(100, 220, 0, 220);
+// line(100, 360, 0, 360);
+// line(100, 160, 200, 160); 
+// line(100, 290, 200, 290); 
+// line(380, 360, 200, 360); 
+// line(200, 2, 200, 360); 
+// line(380, 2, 380, 360); 
+// line(290, 430, 290, 360);
